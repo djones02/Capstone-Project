2cb0236d091f699c4a20f24d685404330bbb5ef8
footballjones <footballjones308@gmail.com>
2023-07-07 14:02:34 -0400

    Added framework for front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Profile from './pages/Profile.jsx'
+import Listings from './pages/Listings.jsx'
+import ListingById from './pages/ListingById.jsx'
+import Users from './pages/Users.jsx'
+import UserById from './pages/UserById.jsx'
+import Cart from './pages/Cart.jsx'
+import Orders from './pages/Orders.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' index element={<Home />} />
+        <Route path='/login' index element={<Login />} />
+        <Route path='/signup' index element={<Signup />} />
+        <Route path='/profile' index element={<Profile />} />
+        <Route path='/listings' index element={<Listings />} />
+        <Route path='/listing/:id' index element={<ListingById />} />
+        <Route path='/users' index element={<Users />} />
+        <Route path='/user/:id' index element={<UserById />} />
+        <Route path='/cart' index element={<Cart />} />
+        <Route path='/orders' index element={<Orders />} />
+      </Routes>
+    </div>
   )
 }
 
-export default App
+
---
+++ client/src/pages/Listings.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Listings() {
+  return (
+    <div>Listings</div>
+  )
+}
+
